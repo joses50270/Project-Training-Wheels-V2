0daff9fb5fe0d97341717d8cc44bb74304823996
Asarel Castellanos <asarelcastellanos@Asarels-MacBook-Pro.local>
2018-07-27 12:39:33 -0700

    Alignment of buttons and title.

--- src/app/components/product-development/product-development.component.ts
+++ src/app/components/product-development/product-development.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AgileDevelopmentComponent } from 'src/app/components/product-development/agile-development/agile-development.component';
+import { DesignThinkingComponent } from 'src/app/components/product-development/design-thinking/design-thinking.component';
+import { ProblemStatementsComponent } from 'src/app/components/product-development/problem-statements/problem-statements.component';
+
 
 @Component({
   selector: 'app-product-development',
@@ -7,9 +13,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDevelopmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  toProblemStatements() {
+    this.router.navigateByUrl('Product-Development/Problem-Statements');
+  }
+
+  toAgileDevelopment() {
+    this.router.navigateByUrl('Product-Development/Agile-Development');
+  }
+
+  toDesignThinking() {
+    this.router.navigateByUrl('Product-Development/Design-Thinking');
   }
 
 }
--- src/app/components/tech-curriculum/tech-curriculum.component.ts
+++ src/app/components/tech-curriculum/tech-curriculum.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+import { AngularComponent } from 'src/app/components/tech-curriculum/angular/angular.component';
+import { CssComponent } from 'src/app/components/tech-curriculum/css/css.component';
+import { HtmlComponent } from 'src/app/components/tech-curriculum/html/html.component';
+import { IonicComponent } from 'src/app/components/tech-curriculum/ionic/ionic.component';
+import { MongodbComponent } from 'src/app/components/tech-curriculum/mongodb/mongodb.component';
+import { PythonComponent } from 'src/app/components/tech-curriculum/python/python.component';
 
 @Component({
   selector: 'app-tech-curriculum',
@@ -7,9 +16,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechCurriculumComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  toAngular() {
+    this.router.navigateByUrl('Tech-Curriculum/Angular');
+  }
+
+  toHTML() {
+    this.router.navigateByUrl('Tech-Curriculum/HTML');
+  }
+
+  toCSS() {
+    this.router.navigateByUrl('Tech-Curriculum/CSS');
+  }
+
+  toPython() {
+    this.router.navigateByUrl('Tech-Curriculum/Python');
+  }
+
+  toIonic() {
+    this.router.navigateByUrl('Tech-Curriculum/Ionic');
+  }
+
+  toMongoDB() {
+    this.router.navigateByUrl('Tech-Curriculum/MongoDB')
   }
 
 }
