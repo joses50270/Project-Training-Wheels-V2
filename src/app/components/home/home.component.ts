8eea2f21f359cc6544bf9f002e009c046502aa51
Asarel Castellanos <asarelcastellanos@Asarels-MacBook-Pro.local>
2018-07-27 11:03:27 -0700

    Home page navigation works.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { TechCurriculumComponent } from 'src/app/components/tech-curriculum/tech-curriculum.component';
-// import { YouthDevelopment } from 'src/app/components/'
+import { YouthDevelopmentComponent } from 'src/app/components/youth-development/youth-development.component';
+import { ProductDevelopmentComponent } from 'src/app/components/product-development/product-development.component';
 import { CultureComponent } from 'src/app/components/culture/culture.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +12,25 @@ import { CultureComponent } from 'src/app/components/culture/culture.component';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  toTechCurriculum() {
+    this.router.navigateByUrl('Tech-Curriculum');
+  }
+
+  toYouthDevelopment() {
+    this.router.navigateByUrl('Youth-Development');
+  }
+
+  toTXTCulture() {
+    this.router.navigateByUrl('TXT-Culture');
+  }
+
+  toProductDevelopment() {
+    this.router.navigateByUrl('Product-Development');
   }
 
 }
